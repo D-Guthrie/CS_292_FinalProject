65d42b7c33a29639077b36d02ef00687d0ddfa9c
David Guthrie <david.s.guthrie@gmail.com>
2016-11-23 23:43:29 -0500

    Started implementing Book class

---
+++ FinalProject_Guthrie/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_Guthrie
+{
+    class Book
+    {
+        private string _ISBN;
+        private string _author;
+        private string _title;
+        private DateTime _datePublished;
+        private string _category;
+        private string _subcategories;
+
+        public string ISBN
+        {
+            get { return _ISBN; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        public DateTime DatePublished
+        {
+            get { return _datePublished; }
+        }
+
+        public string Category
+        {
+            get { return _category; }
+        }
+
+        public string SubCategories
+        {
+            get { return _subcategories; }
+        }
+    }
+}
